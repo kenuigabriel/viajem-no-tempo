31ff33d670522a7157ccdb16833af866c0d36591
kenuigabriel <kenui.bandeira@escola.pr.gov.br>
2025-08-19 09:01:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Você acaba de descobrir uma máquina do tempo em um antigo laboratório abandonado. Ela está pronta para te levar para qualquer época, mas antes de partir, qual época você escolhe explorar primeiro?",
+        alternativas: [
+            {
+                texto: "Viajar para o Egito Antigo para ver as pirâmides sendo construídas",
+                afirmacao: [
+                    "No final da sua jornada, ",
+                    "Ao concluir sua jornada,"
+                ]
+            },
+            {
+                texto: "Viajar para o futuro, para ver como o mundo vai ser daqui a 100 anos.",
+                afirmacao: [
+                    "as escolhas que você fez",
+                    "cada passo dado através do tempo"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você chega à Roma Antiga, onde um grande evento está acontecendo no Coliseu: gladiadores lutam pela sobrevivência. O que você decide fazer?",
+        alternativas: [
+            {
+                texto: "Assistir ao evento como um espectador e tentar aprender mais sobre a cultura romana.",
+                afirmacao: [
+                    "ao longo das viagens no tempo",
+                    "refletirá de maneira única"
+                ]
+            },
+            {
+                texto: "Se infiltrar na arena e tentar lutar com os gladiadores.",
+                afirmacao: [
+                    "terão um grande impacto",
+                    "no tecido da realidade."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você viaja para o ano de 2050, onde o mundo está repleto de inovações tecnológicas. Mas você logo descobre um grande dilema social. Como você lida com essa situação?",
+        alternativas: [
+            {
+                texto: "Apoiar a implementação de IA para ajudar na gestão de recursos e resolver problemas sociais.",
+                afirmacao: [
+                    "no presente e no futuro. ",
+                    "O curso da história,"
+                ]
+            },
+            {
+                texto: "Lutar contra a dependência da tecnologia, tentando promover soluções mais humanas.",
+                afirmacao: [
+                    "Dependendo das suas decisões, ",
+                    "moldado pelas suas escolhas,"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: " Você chega no ano de 3000, onde a humanidade vive em colônias espaciais. Agora, você se depara com uma situação onde é necessário escolher entre salvar uma colônia inteira ou um grupo de astronautas em uma missão de resgate. O que você faz?",
+        alternativas: [
+            {
+                texto: "Escolher salvar a colônia inteira, mesmo sabendo que a missão de resgate pode falhar.",
+                afirmacao: [
+                    "o mundo pode ser",
+                    "poderá resultar em um futuro"
+                ]
+            },
+            {
+                texto: "Optar pelo resgate dos astronautas, priorizando a vida individual sobre a coletiva.",
+                afirmacao: [
+                    "mais avançado tecnologicamente,",
+                    "de inovações extraordinárias,"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você retorna ao presente, mas percebe que suas viagens no tempo deixaram um impacto no mundo. Como você lida com essa situação",
+     alternativas: [
+            {
+                texto: "Decidir usar o conhecimento adquirido para ajudar o mundo a evoluir de maneira mais ética e sustentável.",
+                afirmacao: [
+                    "mais equilibrado, ou até mesmo em uma crise devido às mudanças que você causou.",
+                    "e harmonia entre as civilizações, ou de desafios intensos e crises inesperadas que exigem uma reconstrução do que foi alterado."
+                ]
+            },
+            {
+                texto: " Tentar apagar todas as suas viagens no tempo, para evitar consequências imprevistas.",
+                afirmacao: [
+                    "ij",
+                    "hby"
+            ]
+            }
+        ]
+    },
+];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
+
